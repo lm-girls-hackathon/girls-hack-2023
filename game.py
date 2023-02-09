46df19bdb3cec451c1976195bc61c8d2897d80c7
Sasha Murray <sashakmurray@gmail.com>
2023-02-09 07:33:18 -0500

    Add walls and collisions.

--- game.py
+++ game.py
@@ -1,13 +1,20 @@
 import arcade
 
-SCREEN_LEN = 512
-WINDOW_LEN = int(SCREEN_LEN * 1.25)
+WINDOW_LEN = 512
+BOARD_LEN = 64
+SQUARE_LEN = WINDOW_LEN // BOARD_LEN
 
 
 class Game(arcade.Window):
     def __init__(self):
         super().__init__(WINDOW_LEN, WINDOW_LEN, "Pac Man")
         arcade.set_background_color(arcade.color.ASH_GREY)
+        self.board = [[0 for _ in range(64)] for _ in range(64)]
+        for i in range(BOARD_LEN):
+            for j in range(BOARD_LEN):
+                if i == 50 or j == 50 or i == 20:
+                    self.board[j][i] = 1
+
         self.character = arcade.Sprite(
             "images/character.png",
             center_x=WINDOW_LEN // 2,
@@ -17,14 +24,52 @@ class Game(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
+        self.draw_squares()
         self.character.draw()
 
+    def draw_squares(self):
+        for i in range(BOARD_LEN):
+            for j in range(BOARD_LEN):
+                x = map_position(i)
+                y = map_position(j)
+                if self.board[j][i] == 1:
+                    arcade.draw_lrtb_rectangle_filled(
+                        x,
+                        x + SQUARE_LEN,
+                        y + SQUARE_LEN,
+                        y,
+                        color=arcade.color.BLACK,
+                    )
+
     def on_key_press(self, symbol: int, modifiers: int):
-        if symbol == arcade.key.RIGHT:
-            self.character.center_x += 5
-        elif symbol == arcade.key.LEFT:
-            self.character.center_x -= 5
-        elif symbol == arcade.key.UP:
-            self.character.center_y += 5
-        elif symbol == arcade.key.DOWN:
-            self.character.center_y -= 5
+        x_min = int(self.character.center_x - self.character.width / 2)
+        x_max = int(self.character.center_x + self.character.width / 2)
+        y_min = int(self.character.center_y - self.character.height / 2)
+        y_max = int(self.character.center_y + self.character.height / 2)
+        move = True
+        if symbol in (arcade.key.RIGHT, arcade.key.UP):
+            diff = 5
+        else:
+            diff = -5
+        if symbol in (arcade.key.RIGHT, arcade.key.LEFT):
+            for i in range(x_min + diff, x_max + diff):
+                for j in range(y_min, y_max):
+                    if self.board[map_coord(j)][map_coord(i)] == 1:
+                        move = False
+            if move:
+                self.character.center_x += diff
+        elif symbol in (arcade.key.UP, arcade.key.DOWN):
+            for i in range(x_min, x_max):
+                for j in range(y_min + diff, y_max + diff):
+                    if self.board[map_coord(j)][map_coord(i)] == 1:
+                        move = False
+            if move:
+                self.character.center_y += diff
+
+
+def map_position(x):
+    return x * SQUARE_LEN
+
+
+def map_coord(x):
+    return x // SQUARE_LEN
